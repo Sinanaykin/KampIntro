eab0c856c3d634cee6400213a139b92d33341a2f
Sinanaykin <sinanaykinnn@gmail.com>
2021-05-14 13:45:22 +0300

    İNHERİTANCE

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+   public  class ProductManager
+    {
+        public void Add(Product product)//Ürün ekliceksek ürün göndermeliyiz.Product türünde  bir product göndermeliyiz int id gibi.
+                                        //ilk Product ı yazmak yerine teker teker int Id,string ProductName ... gibi özelliklerin hepsini yzabiliriz
+                                        // ama Encapsulation  kullanımı olarak hepsi Product altında toplanmış bu yüzden Product yolluyoruz direk
+        {
+            Console.WriteLine(product.ProductName + "eklendi");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + "güncellendi");
+        }
+     }
+}
---
+++ Oop2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Oop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+;            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "SİNAN";
+            musteri1.Soyadi = "AYKIN";
+            musteri1.TcNo = "12345678986";
+
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "45628";
+            musteri2.SirketAdi = "kodlama.io";
+            musteri2.VergiNo = "123465987";
+
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri1);
+            musteriManager.Add(musteri2);
+            musteriManager.Add(musteri3);
+
+        }
+    }
+}
